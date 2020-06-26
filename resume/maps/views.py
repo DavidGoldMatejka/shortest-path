a936533175b56171998c7cb7419827ffbcf16cfc
DavidGoldMatejka <61890180+DavidGoldMatejka@users.noreply.github.com>
2020-06-26 15:14:39 -0700

    Initial Commit

---
+++ resume/maps/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def Map(request):
+    return render(request, 'maps/index.html')
---
+++ resume/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # @ts-ignore
+        return f'{self.user.username} Profile'
