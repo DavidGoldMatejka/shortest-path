c12d712126b6aa41b2e1d8073675cd788031da1d
DavidGoldMatejka <61890180+DavidGoldMatejka@users.noreply.github.com>
2020-06-26 15:55:00 -0700

    Static files

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -16,6 +16,14 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -122,15 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/resume_V4/resume/home/static',
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-STATIC_URL = '/static/'
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "static"),
+    #'/resume_V4/resume/home/static',
+#]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
